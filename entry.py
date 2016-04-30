f09cd3abc7b65a3b04e9aea6bf4178e223ae3ba4
mariamfakihh <mariamfakihh@outlook.com>
2016-04-30 17:19:09 +0300

    Added files via upload

--- entry.py
+++ entry.py
@@ -4,7 +4,7 @@ db=MySQLdb.connect('localhost','root','root','map')
 curs=db.cursor()
 def insert(name,pic):
     try:
-        curs.execute("INSERT INTO tyr VALUES('4',",name,",",pic,")")
+        curs.execute("""INSERT INTO tyr VALUES('%s','%s')"""%(name,pic))
         db.commit()
         print "Data committed"
     except:
@@ -18,16 +18,15 @@ panel1=Label(frame,textvariable=var)
 var.set("Street name:")
 panel1.grid(row=0,column=0)
 panel2=Label(frame,text="image name:")
-panel2.pack(side=LEFT)
+panel2.grid(row=1,column=0)
 ent1=Entry(frame)
 ent1.grid(row=0,column=1)
 ent2=Entry(frame)
 ent2.grid(row=1,column=1)
 button=Button(frame,text="insert in the map",bg="white",
-              command=lambda:insert(ent1['text'],ent2['text']))
-#button.pack()
+              command=lambda:insert(ent1.get(),ent2.get()))
 
 button.grid(row=2,column=1)
 root.grid(200,100,320,100)
-
+root.title("entry")
 root.mainloop()
